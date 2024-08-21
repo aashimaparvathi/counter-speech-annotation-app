ded9824f91a665521004fe4a172e9479042847c3
Aashima <aashimaparvathi@gmail.com>
2024-08-21 15:52:32 +0100

    streamlit cloud test 3

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from collections import defaultdict
 data = pd.read_csv('data/intentconanv2/40-per-target-sample.csv')
 
 user_mapping = {
-    'CSAT1758': (0, 40),
-    'CSAT3968': (40, 80),
+    'CSAT1758': (0, 10),
+    'CSAT3968': (10, 20),
     'CSAT1245': (80, 120),
     'CSAT9877': (120, 160),
     'CSAT1290': (160, 200),
